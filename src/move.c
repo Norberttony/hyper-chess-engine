9a6c6b4a1e76238ddb50793db3b9b3fa90720139
Norberttony <norberttony@icloud.com>
2024-12-04 09:32:03 -0500

    woops fix

--- src/move.c
+++ src/move.c
@@ -1,18 +1,6 @@
 
 #include "move.h"
 
-const char* squareNames[] =
-{
-    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
-};
-
 const int move_typeMask     = 0b0000000000000000000000000111; // piece performing move
 const int move_fromMask     = 0b0000000000000000000111111000; // from
 const int move_toMask       = 0b0000000000000111111000000000; // to
